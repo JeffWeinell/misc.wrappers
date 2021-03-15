abfaaf01ae8082bb37eb3e4a688bc964991c7e58
Jeffrey Weinell <jweine2@gmail.com>
2021-03-15 16:34:42 -0500

    Update genind2diffs.R

--- R/genind2diffs.R
+++ R/genind2diffs.R
@@ -2,7 +2,6 @@
 #' 
 #' Generates a diffs matrix from a genind object
 #' 
-#' 
 #### This function supercedes bed2diffs_v1 and bed2diffs_v2
 #' @param genind.obj Input genind object (see adegenet package for description of genind class). Non-biallelic sites are dropped if they exist.
 #' @param ploidy Integer indicating the ploidy of individuals
@@ -11,7 +10,7 @@
 #' @return A list of length three   with (1) a numeric matrix corresponding to the Diff matrix used for EEMS, (2) the number of individuals, (3) number of biallelic sites in genind.obj
 #' @export genind2diffs
 genind2diffs <- function(genind.obj,ploidy=2,include.indv.names=F,output.file=NULL){
-	gen         <- genind.obj
+	gen        <- genind.obj
 	adegenet::ploidy(gen) <- ploidy
 	stopifnot(identical(gen@type, 'codom'))
 	Geno       <- gen@tab
@@ -29,49 +28,52 @@ genind2diffs <- function(genind.obj,ploidy=2,include.indv.names=F,output.file=NU
 	nSites <- ncol(Geno)
 	# Missing data sites
 	Miss   <- is.na(Geno)
-	## Impute NAs with the column means (= twice the allele frequencies); a row of means; a matrix with nIndiv identical rows of means
+	### Impute NAs with the column means of Geno
+	# This is a numeric matrix with dimensions the same as Geno; all values in a column are the same and equal to the mean of non-NA values in the corresponding Geno column (= twice the allele frequencies)
 	Mean   <- matrix(colMeans(Geno, na.rm = TRUE),nrow = nIndiv, ncol = nSites, byrow = TRUE)
-	## Set the means that correspond to observed genotypes to 0
-	Mean[Miss == 0] <- 0
-	## Set the missing genotypes to 0 (used to be NA) 
-	Geno[Miss == 1] <- 0
+	### Set the means in Mean to zero for observed genotypes, because these don't need to be imputed.
+	# Mean[Miss == FALSE] <- 0
+	Mean[!Miss] <- 0
+	### Set the missing genotypes to 0 (used to be NA).
+	Geno[Miss]  <- 0
+	### Missing genotypes will take on the value from Mean and values at observed genotypes do not change.
 	Geno     <- Geno + Mean
 	## Compute similarities
-	Sim      <- Geno %*% t(Geno) / nSites
-	## self-similarities
+	Sim      <- (Geno %*% t(Geno)) / nSites
+	## Self-similarities
 	SelfSim  <- diag(Sim)
 	## vector of 1s
 	vector1s <- rep(1, nIndiv)
-	## This chunk generates a `diffs` matrix (version 1)
-	Diffs1 <- SelfSim %*% t(vector1s) + vector1s %*% t(SelfSim) - 2 * Sim
+	## Uses some linear algebra to generate a `diffs` matrix (version 1)
+	Diffs1 <- (SelfSim %*% t(vector1s)) + (vector1s %*% t(SelfSim)) - (2 * Sim)
 	rownames(Diffs1) <- colnames(Diffs1)
 	######
-	Diffs2 <- matrix(0, nIndiv, nIndiv)
-	for (i in seq(nIndiv - 1)) {
-		for (j in seq(i + 1, nIndiv)) {
-			x <- Geno[i, ]
-			y <- Geno[j, ]
-			Diffs2[i, j] <- mean((x - y)^2, na.rm = TRUE)
-			Diffs2[j, i] <- Diffs2[i, j]
-		}
-	}
-	colnames(Diffs2) <- colnames(Diffs1)
-	rownames(Diffs2) <- rownames(Diffs1)
+	# Diffs2 <- matrix(0, nIndiv, nIndiv)
+	#for (i in seq(nIndiv - 1)) {
+	#	for (j in seq(i + 1, nIndiv)) {
+	#		x <- Geno[i, ]
+	#		y <- Geno[j, ]
+	#		Diffs2[i, j] <- mean((x - y)^2, na.rm = TRUE)
+	#		Diffs2[j, i] <- Diffs2[i, j]
+	#	}
+	#}
+	# colnames(Diffs2) <- colnames(Diffs1)
+	# rownames(Diffs2) <- rownames(Diffs1)
 	### Round diffs to six digits
 	Diffs1 <- round(Diffs1, digits = 6)
-	Diffs2 <- round(Diffs2, digits = 6)
+#	 Diffs2 <- round(Diffs2, digits = 6)
 	### Check which Diffs matrix to use by taking the eigenvalue
-	eigvals.Diffs_v1 <- sort(round(eigen(Diffs1)$values, digits = 2))
-	eigvals.Diffs_v2 <- sort(round(eigen(Diffs2)$values, digits = 2))
-	if(length(which(eigvals.Diffs_v1>0))==1){
-		diffs <- Diffs1
-	} else {
-		if(length(which(eigvals.Diffs_v2>0))==1){
-		diffs <- Diffs2
-		} else {
-		stop("No Diffs matrix with one positive eigenvalue")
-		}
-	}
+#	eigvals.Diffs_v1 <- sort(round(eigen(Diffs1)$values, digits = 2))
+#	 eigvals.Diffs_v2 <- sort(round(eigen(Diffs2)$values, digits = 2))
+	# if(length(which(eigvals.Diffs_v1>0))==1){
+	diffs <- Diffs1
+	# } else {
+	# 	if(length(which(eigvals.Diffs_v2>0))==1){
+#	 	diffs <- Diffs2
+	# 	} else {
+	# 	stop("No Diffs matrix with one positive eigenvalue")
+	# 	}
+	# }
 	if(!include.indv.names){
 		rownames(diffs) <- NULL
 		colnames(diffs) <- NULL
